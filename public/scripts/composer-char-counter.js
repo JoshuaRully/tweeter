76681d12ef26757adee8d8486fb14fd9cd3c9860
Joshua Rully <joshrully@gmail.com>
2021-02-23 22:05:28 +0000

    added CSS styling to output if character limit is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,15 @@
 // backup usage of .ready() in case defer is forgotten or removed
 $(document).ready( function() {
-  const charCount = ".new-tweet textarea";
+  const charCount = '.new-tweet textarea';
   const updateCount = function() {
     let remainder = 140 - $(this).val().length;
-    $(".counter").text(remainder);
+    $('.counter').text(remainder);
+    if (remainder < 0) {
+      $('output').attr('id', 'alert-text');
+    } else {
+      $('output').removeAttr('id');
+    }
+    
   }
   $(charCount).keyup(updateCount);
 });
